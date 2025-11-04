3a4a36967cf8273008eacffc247a6c8e055454e3
saad-78 <smomin1008@gmail.com>
2025-11-04 23:20:25 +0530

    updated again tsconfig.ts in fe

--- apps/excalidraw-frontend/app/config.ts
+++ apps/excalidraw-frontend/app/config.ts
@@ -1,11 +1,2 @@
-// src/config.ts
-
-const isDev = process.env.NODE_ENV === 'development';
-
-export const Backend_URL = isDev 
-  ? "http://localhost:3001"
-  : "https://excalidraw-http-backend.onrender.com";
-
-export const WS_URL = isDev 
-  ? "ws://localhost:10000/ws"
-  : "wss://excalidraw-sozx.onrender.com/ws";
+export const Backend_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "https://excalidraw-http-backend.onrender.com";
+export const WS_URL = process.env.NEXT_PUBLIC_WS_URL || "wss://excalidraw-sozx.onrender.com/ws";
