f7814d13919fd993c47f3ba072f096a11045b470
saad-78 <smomin1008@gmail.com>
2025-11-04 23:43:19 +0530

    updated the url and params

--- apps/excalidraw-frontend/app/canvas/[roomId]/page.tsx
+++ apps/excalidraw-frontend/app/canvas/[roomId]/page.tsx
@@ -1,12 +1,14 @@
 import { RoomCanvas } from "@/app/components/RoomCanvas"
 import { ProtectedRoute } from "@/app/components/ProtectedRoute"
 
+
 export default async function CanvasPage({ params }: {
-    params: {
+    params: Promise<{
         roomId: string
-    }
+    }>
 }) {
-    const roomId = (await params).roomId
+    const { roomId } = await params
+
 
     return (
         <ProtectedRoute>
--- apps/excalidraw-frontend/app/config.ts
+++ apps/excalidraw-frontend/app/config.ts
@@ -1,2 +1,2 @@
 export const Backend_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "https://excalidraw-http-backend.onrender.com";
-export const WS_URL = process.env.NEXT_PUBLIC_WS_URL || "wss://excalidraw-sozx.onrender.com/ws";
+export const WS_URL = process.env.NEXT_PUBLIC_WS_URL || "https://excalidraw-sozx.onrender.com/api/keep-alive";
